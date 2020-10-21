9d53a8081e738dbc1719728dc7ff85788d20a7b8
inksharedsolutions <webdev.inkshared@gmail.com>
2020-10-21 11:46:26 +0800

    banner changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = (props) => {
 				<span className="banr-spn-highlight">This is the best day of my life.</span>
 
 				<p className="paragraph-content">
-				As The Megalosaur swallowed her husband then turned toward Winona, she screamed “no way you’re having me for dessert you monster, as she fired the laser rifle.
+				As the megalosaur swallowed her husband then turned toward Winona, she screamed “no way you’re having me for dessert you monster, as she fired the laser rifle.
 				</p>
 
 				<Link
